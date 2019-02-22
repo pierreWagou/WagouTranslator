cedb5685976b0d61286be841ff140605a7eb1ae3
CaillouxZinzin <pierre.romon@etu.utc.fr>
2019-02-22 17:44:05 +0200

    translator working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Form, FormGroup, Input, Label, Button} from 'reactstrap'
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.emptySource = false
+    this.emptyTarget = false
+    this.state = {
+      wordSource: "",
+      wordTarget: "",
+      dico: []
+    }
+  }
+
+  componentDidMount() {
+    this.loadDico()
+  }
+
+  loadDico() {
+    axios
+      .get("http://localhost:8000/api/words")
+      .then(res => this.setState({dico: res.data}))
+      .catch(err => console.log(err))
+  }
+
+  reset() {
+    this.emptySource = false
+    this.emptyTarget = false
+    this.setState({wordSource: "", wordTarget: ""})
+  }
+
+  textInputSourceChanged(text) {
+    this.emptyTarget = true
+    this.setState({wordSource: text.target.value})
+  }
+
+  textInputTargetChanged(text) {
+    this.emptySource = true
+    this.setState({wordTarget: text.target.value})
+  }
+
+  translate() {
+    for (var i=0;i<this.state.dico.length;i++) {
+      if(this.state.dico[i].en===this.state.wordSource && this.emptyTarget) {
+        this.setState({wordTarget: this.state.dico[i].wg})
+      }
+      if(this.state.dico[i].wg===this.state.wordTarget && this.emptySource) {
+        this.setState({wordSource: this.state.dico[i].en})
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Wagou-Translator</h1>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <Form>
+                  <FormGroup>
+                    <Label for="source">English</Label>
+                    <Input
+                      type="text"
+                      name="source"
+                      placeholder="English word"
+                      onChange={text => this.textInputSourceChanged(text)}
+                      defaultValue={this.state.wordSource}
+                      readOnly={this.emptySource}
+                    />
+                </FormGroup>
+                  <FormGroup>
+                    <Label for="target">Wagou</Label>
+                    <Input
+                      type="text"
+                      name="target"
+                      placeholder="Wagou ysuwiwagouhoh"
+                      onChange={text => this.textInputTargetChanged(text)}
+                      defaultValue={this.state.wordTarget}
+                      readOnly={this.emptyTarget}
+                    />
+                </FormGroup>
+                  <FormGroup>
+                    <Button color="danger" onClick={() => this.reset()}>Reset</Button>
+                    <Button color="success" onClick={() => this.translate()}>Translate</Button>
+                  </FormGroup>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
   }
 }
 
